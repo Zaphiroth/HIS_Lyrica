cc093bf56a0ba8ee1254017e5026969959c1bbaf
Zaphiroth <zliu@pharbers.com>
2020-11-10 19:04:23 +0800

    readin 2020

--- 04_Codes/01_Readin_Raw_Data.R
+++ 04_Codes/01_Readin_Raw_Data.R
@@ -12,7 +12,15 @@ ReadinFunc <- function(filename) {
   
   sheet.names <- excel_sheets(filename)
   
-  sheet.list <- lapply(sheet.names, read_excel, path = filename)
+  sheet.list <- lapply(sheet.names, read_excel, path = filename) %>% 
+    lapply(mutate, 
+           VISIT_ID = as.numeric(VISIT_ID), 
+           VISIT_DATE = as.character(VISIT_DATE), 
+           AGE = as.numeric(AGE), 
+           AMOUNT = as.numeric(AMOUNT), 
+           COSTS = as.numeric(COSTS), 
+           ADMISSION_DATE_TIME = as.character(ADMISSION_DATE_TIME), 
+           DISCHARGE_DATE_TIME = as.character(DISCHARGE_DATE_TIME))
   
   names(sheet.list) <- sheet.names
   
@@ -21,38 +29,31 @@ ReadinFunc <- function(filename) {
 
 
 ##---- Outpatient data ----
-filename.outpatient <- list.files('02_Inputs/门诊数据v2.0(2018-2019)', 
-                                  full.names = TRUE)
+filename.outpatient <- list.files('02_Inputs/门诊数据v2.0(2018-2019)', full.names = TRUE) %>% 
+  c(list.files('02_Inputs/门诊数据v2.0(2020)', full.names = TRUE))
 
 file.list.outpatient <- map(filename.outpatient, ReadinFunc)
 
 data.outpatient <- lapply(file.list.outpatient, bind_rows) %>% 
   bind_rows()
 
-write.csv(data.outpatient, '03_Outputs/01_门诊信息合并(2018-2019).csv')
+write_feather(data.outpatient, '03_Outputs/01_Outpatient_Raw.feather')
 
 
 ##---- Inhospital data ----
-filename.inhospital <- list.files('02_Inputs/住院信息v2.0(2018-2019)', 
-                                  full.names = TRUE)
+filename.inhospital <- list.files('02_Inputs/住院信息v2.0(2018-2019)', full.names = TRUE) %>% 
+  c(list.files('02_Inputs/住院数据v2.0(2020)', full.names = TRUE))
 
 file.list.inhospital <- map(filename.inhospital, ReadinFunc)
 
 data.inhospital <- lapply(file.list.inhospital, bind_rows) %>% 
-  lapply(mutate, 
-         VISIT_ID = as.numeric(VISIT_ID), 
-         AGE = as.numeric(AGE), 
-         AMOUNT = as.numeric(AMOUNT), 
-         COSTS = as.numeric(COSTS)) %>% 
   bind_rows()
 
-write.csv(data.inhospital, '03_Outputs/01_住院信息合并(2018-2019).csv')
+write_feather(data.inhospital, '03_Outputs/01_Inhospital_Raw.feather')
 
 
 ##---- Bind ----
-data.his <- data.outpatient %>% 
-  mutate(VISIT_DATE = as.character(VISIT_DATE)) %>% 
-  bind_rows(data.inhospital) %>% 
+data.his <- bind_rows(data.outpatient, data.inhospital) %>% 
   mutate(VISIT_DATE = stri_sub(VISIT_DATE, 1, 10))
 
-write_feather(data.his, '03_Outputs/01_门诊&住院信息.feather')
+write_feather(data.his, '03_Outputs/01_HIS_Raw.feather')
