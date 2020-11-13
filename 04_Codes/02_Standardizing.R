c90196236023e12330d8d3301ebd22dd5405c782
Zaphiroth <zliu@pharbers.com>
2020-11-13 22:49:16 +0800

    nep

--- 04_Codes/02_Standardizing.R
+++ 04_Codes/02_Standardizing.R
@@ -7,10 +7,16 @@
 
 
 ##---- Standardizing ----
+## sample selection
+hospital.sample <- read.xlsx('02_Inputs/样本医院名单-30家.xlsx')
+
 dept.std.mapping <- read.xlsx('02_Inputs/HIS科室清洗_veeva标准.xlsx', sheet = 3, cols = c(1, 6))
+nep.mapping <- read.xlsx('02_Inputs/清洗规则_应用版3.xlsx', cols = c(2, 3))
 
 {
   data.standard <- data.his %>% 
+    filter(HCODE %in% hospital.sample$HCODE) %>% 
+    left_join(dept.std.mapping, by = c('DEPT_NAME' = 'dept_name')) %>% 
     mutate(
       Mol_std = case_when(
         grepl('阿米替林', DRUG_NAME) ~ '阿米替林', 
@@ -44,43 +50,55 @@ dept.std.mapping <- read.xlsx('02_Inputs/HIS科室清洗_veeva标准.xlsx', shee
         TRUE ~ NA_character_
       ), 
       Dept_std = case_when(
-        grepl('急诊', DEPT_NAME) ~ '急诊科'
-        grepl('儿科', DEPT_NAME) ~ '儿科'
-        grepl('疼痛|麻醉', DEPT_NAME) ~ '疼痛科'
-        grepl('皮', DEPT_NAME) ~ '皮肤科'
-        grepl('运动医学|骨质疏松', DEPT_NAME) ~ '骨科'
-        grepl('外科', DEPT_NAME) ~ '外科'
-        grepl('内分泌|消化内|血液内分泌|代谢病', DEPT_NAME) ~ '内分泌科'
-        grepl('精神|心理', DEPT_NAME) ~ '精神心理科'
-        grepl('神经内', DEPT_NAME) ~ '神经内科'
-        grepl('肿瘤外', DEPT_NAME) ~ '肿瘤外科'
-        grepl('肿瘤内', DEPT_NAME) ~ '肿瘤内科'
-        grepl('心脏中心|心内', DEPT_NAME) ~ '心内科'
-        grepl('肾内科', DEPT_NAME) ~ '肾内科'
-        grepl('普通内', DEPT_NAME) ~ '普通内科'
-        grepl('泌尿肿瘤外', DEPT_NAME) ~ '泌尿外科'
-        grepl('风湿免疫|血液风湿', DEPT_NAME) ~ '风湿免疫科'
-        grepl('老干', DEPT_NAME) ~ '老干科',  
+        grepl('急诊', DEPT_NAME) ~ '急诊科', 
+        grepl('儿科', DEPT_NAME) ~ '儿科', 
+        grepl('疼痛|麻醉', DEPT_NAME) ~ '疼痛科', 
+        grepl('皮', DEPT_NAME) ~ '皮肤科', 
+        grepl('运动医学|骨质疏松', DEPT_NAME) ~ '骨科', 
+        grepl('外科', DEPT_NAME) ~ '外科', 
+        grepl('内分泌|消化内|血液内分泌|代谢病', DEPT_NAME) ~ '内分泌科', 
+        grepl('精神|心理', DEPT_NAME) ~ '精神心理科', 
+        grepl('神经内', DEPT_NAME) ~ '神经内科', 
+        grepl('肿瘤外', DEPT_NAME) ~ '肿瘤外科', 
+        grepl('肿瘤内', DEPT_NAME) ~ '肿瘤内科', 
+        grepl('心脏中心|心内', DEPT_NAME) ~ '心内科', 
+        grepl('肾内科', DEPT_NAME) ~ '肾内科', 
+        grepl('普通内', DEPT_NAME) ~ '普通内科', 
+        grepl('泌尿肿瘤外', DEPT_NAME) ~ '泌尿外科', 
+        grepl('风湿免疫|血液风湿', DEPT_NAME) ~ '风湿免疫科', 
+        grepl('老干', DEPT_NAME) ~ '老干科', 
         TRUE ~ NA_character_
       ), 
+      Dept_std1 = case_when(
+        grepl('急诊', `科室清洗结果`) ~ '急诊科', 
+        grepl('儿科', `科室清洗结果`) ~ '儿科', 
+        grepl('疼痛|麻醉', `科室清洗结果`) ~ '疼痛科', 
+        grepl('皮', `科室清洗结果`) ~ '皮肤科', 
+        grepl('运动医学|骨质疏松', `科室清洗结果`) ~ '骨科', 
+        grepl('外科', `科室清洗结果`) ~ '外科', 
+        grepl('内分泌|消化内|血液内分泌|代谢病', `科室清洗结果`) ~ '内分泌科', 
+        grepl('精神|心理', `科室清洗结果`) ~ '精神心理科', 
+        grepl('神经内', `科室清洗结果`) ~ '神经内科', 
+        grepl('肿瘤外', `科室清洗结果`) ~ '肿瘤外科', 
+        grepl('肿瘤内', `科室清洗结果`) ~ '肿瘤内科', 
+        grepl('心脏中心|心内', `科室清洗结果`) ~ '心内科', 
+        grepl('肾内科', `科室清洗结果`) ~ '肾内科', 
+        grepl('普通内', `科室清洗结果`) ~ '普通内科', 
+        grepl('泌尿肿瘤外', `科室清洗结果`) ~ '泌尿外科', 
+        grepl('风湿免疫|血液风湿', `科室清洗结果`) ~ '风湿免疫科', 
+        grepl('老干', `科室清洗结果`) ~ '老干科', 
+        TRUE ~ NA_character_
+      ), 
+      flag_dept = if_else(is.na(Dept_std), 0, 1), 
       Diag_std = case_when(
-        grepl('疱|疹', DIAG_DESC) ~ '疱疹', 
+        grepl('疱疹', DIAG_DESC) ~ '疱疹', 
         grepl('肿瘤|癌', DIAG_DESC) ~ '肿瘤/癌症', 
+        grepl('糖尿病', DIAG_DESC) & grepl('神经', DIAG_DESC) ~ '糖尿病周围神经病变', 
         grepl('糖尿病', DIAG_DESC) ~ '糖尿病', 
-        grepl('术', DIAG_DESC) & grepl('脊|颈|腰', DIAG_DESC) ~ '腰椎脊疾病术后', 
-        grepl('术', DIAG_DESC) & grepl('关节', DIAG_DESC) ~ '关节疾病术后', 
-        grepl('术', DIAG_DESC) & grepl('骨折', DIAG_DESC) ~ '骨折术后', 
-        grepl('术', DIAG_DESC) & grepl('创伤', DIAG_DESC) ~ '创伤术后', 
-        grepl('术', DIAG_DESC) ~ '其他疾病术后', 
-        grepl('脊髓炎', DIAG_DESC) ~ '脊髓炎', 
-        grepl('脊髓损伤', DIAG_DESC) ~ '脊髓损伤', 
-        grepl('脊髓创伤', DIAG_DESC) ~ '脊髓损伤', 
-        grepl('颈椎压迫', DIAG_DESC) ~ '颈椎压迫', 
-        grepl('脊|颈|腰', DIAG_DESC) ~ '其他腰/颈/脊相关疾病', 
         grepl('慢性偏头痛', DIAG_DESC) ~ '慢性偏头痛', 
         grepl('头痛', DIAG_DESC) & grepl('慢性紧张', DIAG_DESC) ~ '慢性紧张型头痛', 
         grepl('头痛', DIAG_DESC) & grepl('慢性丛集', DIAG_DESC) ~ '慢性丛集性头痛', 
-        grepl('头痛', DIAG_DESC) & grepl('慢性', DIAG_DESC) ~ '其他慢性头痛', 
+        grepl('慢性头痛', DIAG_DESC) ~ '其他慢性头痛', 
         grepl('偏头痛', DIAG_DESC) ~ '偏头痛', 
         grepl('头痛', DIAG_DESC) & grepl('紧张', DIAG_DESC) ~ '紧张型头痛', 
         grepl('头痛', DIAG_DESC) & grepl('丛集', DIAG_DESC) ~ '丛集性头痛', 
@@ -91,40 +109,71 @@ dept.std.mapping <- read.xlsx('02_Inputs/HIS科室清洗_veeva标准.xlsx', shee
         grepl('头痛', DIAG_DESC) & grepl('药物过量', DIAG_DESC) ~ '药物过量性头痛', 
         grepl('头痛', DIAG_DESC) & grepl('继发性', DIAG_DESC) ~ '其他继发性头痛', 
         grepl('头痛', DIAG_DESC) & grepl('神经', DIAG_DESC) ~ '神经性头痛', 
-        grepl('头痛', DIAG_DESC) ~ '头痛(未指明类型）', 
-        grepl('神经痛', DIAG_DESC) & grepl('三叉', DIAG_DESC) ~ '三叉神经痛', 
-        grepl('神经病变|神经痛', DIAG_DESC) ~ '其他神经痛', 
+        grepl('头痛', DIAG_DESC) ~ '其他头痛', 
+        grepl('神经', DIAG_DESC) & grepl('腰|背', DIAG_DESC) & grepl('疼|痛|麻|损|病', DIAG_DESC) ~ '腰背神经痛', 
+        grepl('神经', DIAG_DESC) & grepl('三叉', DIAG_DESC) & grepl('疼|痛|麻|损|病', DIAG_DESC) ~ '三叉神经痛', 
+        grepl('神经', DIAG_DESC) & grepl('术', DIAG_DESC) & grepl('疼|痛|麻|损|病', DIAG_DESC) ~ '术后神经痛', 
+        grepl('神经', DIAG_DESC) & grepl('关节', DIAG_DESC) & grepl('疼|痛|麻|损|病', DIAG_DESC) ~ '关节神经痛', 
+        grepl('神经', DIAG_DESC) & grepl('痛', DIAG_DESC) ~ '其他神经痛', 
+        grepl('神经', DIAG_DESC) ~ '其他神经疾病', 
+        grepl('痛', DIAG_DESC) & grepl('脊|脊髓|卒中|多发性硬化', DIAG_DESC) ~ '中枢神经病理性疼痛', 
+        grepl('卒中|多发性硬化', DIAG_DESC) ~ '其他中枢神经疾病', 
+        grepl('术', DIAG_DESC) & grepl('脊|颈|腰', DIAG_DESC) ~ '腰脊疾病术后', 
+        grepl('术', DIAG_DESC) & grepl('关节', DIAG_DESC) ~ '关节疾病术后', 
+        grepl('术', DIAG_DESC) & grepl('骨折', DIAG_DESC) ~ '骨折术后', 
+        grepl('术', DIAG_DESC) & grepl('创伤', DIAG_DESC) ~ '创伤术后', 
+        grepl('术', DIAG_DESC) ~ '其他疾病术后', 
+        grepl('腰|颈|脊', DIAG_DESC) ~ '其他腰背痛', 
         grepl('骨折', DIAG_DESC) ~ '骨折', 
         grepl('关节炎', DIAG_DESC) ~ '关节炎', 
-        TRUE ~ NA_character_
+        grepl('幻肢', DIAG_DESC) & grepl('疼|痛', DIAG_DESC) ~ '幻肢痛', 
+        grepl('疼|痛', DIAG_DESC) ~ '其他疼痛', 
+        TRUE ~ '其他疾病'
+      ), 
+      flag_form = if_else(Mol_std == '利多卡因' & !grepl('贴', DRUG_FORM), 0, 1), 
+      Nep = case_when(
+        Diag_std %in% c('疱疹', '糖尿病周围神经病变', '腰背神经痛', 
+                        '三叉神经痛', '术后神经痛', '关节神经痛', '其他神经痛', 
+                        '中枢神经病理性疼痛', '其他腰背痛', '幻肢痛') ~ '明确或高度可能NeP', 
+        Diag_std %in% c('肿瘤/癌症', '糖尿病', '其他头痛', '其他神经疾病', 
+                        '其他中枢神经疾病', '其他疾病术后', '其他腰背痛') & 
+          flag_form == 1 ~ '很大可能NeP', 
+        Diag_std %in% c('腰脊疾病术后', '关节疾病术后', '创伤术后') ~ '很大可能NeP', 
+        Diag_std %in% c('肿瘤/癌症', '糖尿病', '慢性偏头痛', '慢性紧张型头痛', 
+                        '慢性丛集性头痛', '其他慢性头痛', '偏头痛', '紧张型头痛', 
+                        '丛集性头痛', '其他原发性头痛', '创伤后头痛', '颈源性头痛', 
+                        '高血压头痛', '药物过量性头痛', '其他继发性头痛', 
+                        '神经性头痛', '其他头痛', '其他神经疾病', '骨折术后', 
+                        '其他疾病术后', '其他腰背痛', '骨折', '关节炎', '其他疼痛') ~ '有可能NeP', 
+        TRUE ~ '不太可能NeP'
       )
-    ) %>% 
-    left_join(dept.std.mapping, by = c('DEPT_NAME' = 'dept_name')) %>% 
-    mutate(flag_dept = if_else(is.na(Dept_std), 0, 1), 
-           Dept_std1 = case_when(
-             grepl('急诊', `科室清洗结果`) ~ '急诊科'
-             grepl('儿科', `科室清洗结果`) ~ '儿科'
-             grepl('疼痛|麻醉', `科室清洗结果`) ~ '疼痛科'
-             grepl('皮', `科室清洗结果`) ~ '皮肤科'
-             grepl('运动医学|骨质疏松', `科室清洗结果`) ~ '骨科'
-             grepl('外科', `科室清洗结果`) ~ '外科'
-             grepl('内分泌|消化内|血液内分泌|代谢病', `科室清洗结果`) ~ '内分泌科'
-             grepl('精神|心理', `科室清洗结果`) ~ '精神心理科'
-             grepl('神经内', `科室清洗结果`) ~ '神经内科'
-             grepl('肿瘤外', `科室清洗结果`) ~ '肿瘤外科'
-             grepl('肿瘤内', `科室清洗结果`) ~ '肿瘤内科'
-             grepl('心脏中心|心内', `科室清洗结果`) ~ '心内科'
-             grepl('肾内科', `科室清洗结果`) ~ '肾内科'
-             grepl('普通内', `科室清洗结果`) ~ '普通内科'
-             grepl('泌尿肿瘤外', `科室清洗结果`) ~ '泌尿外科'
-             grepl('风湿免疫|血液风湿', `科室清洗结果`) ~ '风湿免疫科'
-             grepl('老干', `科室清洗结果`) ~ '老干科',  
-             TRUE ~ NA_character_
-           ))
+      # Nep = case_when(
+      #   grepl('疱疹|幻肢痛', DIAG_DESC) ~ '明确或高度可能NeP', 
+      #   grepl('糖尿病|病理', DIAG_DESC) &grepl('神经', DIAG_DESC) ~ '明确或高度可能NeP', 
+      #   grepl('脊髓', DIAG_DESC) &grepl('损伤', DIAG_DESC) ~ '明确或高度可能NeP', 
+      #   grepl('神经', DIAG_DESC) & grepl('腰|背|三叉|术|关节', DIAG_DESC) & grepl('疼|痛|麻|损|病', DIAG_DESC) ~ '明确或高度可能NeP', 
+      #   grepl('脊髓|卒中|多发性硬化', DIAG_DESC) &grepl('痛', DIAG_DESC) ~ '明确或高度可能NeP', 
+      #   grepl('腰|颈|脊髓', DIAG_DESC) &grepl('麻|放射', DIAG_DESC) ~ '明确或高度可能NeP', 
+      #   grepl('糖尿病|术|肿瘤|癌|卒中|多发性硬化|腰|颈|脊|头痛|神经', DIAG_DESC) & 
+      #     grepl('阿米替林|度洛西汀|文拉法辛|普瑞巴林|加巴喷丁|卡马西平|奥卡西平', DIAG_DESC) ~ '很大可能NeP', 
+      #   grepl('糖尿病|术|肿瘤|癌|卒中|多发性硬化|腰|颈|脊|头痛|神经', DIAG_DESC) & 
+      #     grepl('利多卡因', DIAG_DESC) & grep('贴', DRUG_FORM) ~ '很大可能NeP', 
+      #   grepl('糖尿病|术|肿瘤|癌|卒中|多发性硬化|腰|颈|脊|头痛|神经', DIAG_DESC) ~ '有可能NeP', 
+      #   TRUE ~ '不太可能NeP'
+      # )
+    )
 }
 
 ## TODO LZ: 检查缺失情况
 write_feather(data.standard, '03_Outputs/02_HIS_Standard.feather')
+
+nep.pivot <- data.standard %>% 
+  distinct(PKEY, VISIT_ID, Nep) %>% 
+  count(Nep) %>% 
+  mutate(Nep_prop = round(n / sum(n, na.rm = TRUE), 4)) %>% 
+  select(-n)
+
+write.xlsx(nep.pivot, '03_Outputs/02_Nep_Patients_Proportation.xlsx')
 
 ## diagnosis
 unmatched.diag <- data.standard %>% 
